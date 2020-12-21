38a6427825091d1721f08b038798e19bd448d9ba
phancongtaidhsp <phancongtaicntt2@gmail.com>
2020-12-21 14:53:36 +0700

    fix

--- bai2/app.js
+++ bai2/app.js
@@ -2,7 +2,6 @@ let length = document.querySelectorAll('#bxslider .slide-inner .slide-item').len
 let current = 0;
 var carousel = $('.slide-inner'),
   slideWidth = 748;
-stack = 0;
 
 let htmlSLideIndicators = ``;
 for (let i = 0; i < length; i++) {
@@ -16,26 +15,8 @@ document.querySelector('#bxslider').appendChild(ol);
 $('.slide-indicators li').click(function () {
   if (carousel.hasClass('transition')) return;
   let data = $(this).data('slideTo');
-  resetStack();
   SlideTo(current, data);
 });
-
-function resetStack() {
-  if (stack < 0) {
-    for (let i = 0; i < length - Math.abs(stack); i++) {
-      $('.slide-item:first').before($('.slide-item:last'));
-    }
-    carousel.css('transform', 'translateX(' + (length - Math.abs(stack)) * -slideWidth + 'px)');
-    stack = 0;
-  }
-  if (stack > 0) {
-    for (let i = 0; i < length - Math.abs(stack); i++) {
-      $('.slide-item:last').after($('.slide-item:first'))
-    }
-    carousel.css('transform', 'translateX(' + Math.abs(stack - 1) * -slideWidth + 'px)');
-    stack = 0;
-  }
-}
 
 $('a[href="#bxslider"').click(function () {
   if (carousel.hasClass('transition')) return;
@@ -44,41 +25,19 @@ $('a[href="#bxslider"').click(function () {
   if (length < 2) return;
   if (data === 'next') {
     let next = current + 1;
-    if (stack < 0) resetStack();
     if (next === length) {
       next = 0;
-      stack = 1;
       SlideRight(current, next);
     }
-    else {
-      if (stack > 0) {
-        stack++;
-        SlideRight(current, next)
-      }
-      else {
-        SlideTo(current, next)
-      }
-      if (Math.abs(stack) === length) stack = 0;
-    }
+    else SlideTo(current, next)
   }
   else {
     let prev = current - 1;
-    if (stack > 0) resetStack();
     if (prev < 0) {
       prev = length - 1;
-      stack = -1;
       SlideLeft(current, prev)
     }
-    else {
-      if (stack < 0) {
-        stack--;
-        SlideLeft(current, prev)
-      }
-      else {
-        SlideTo(current, prev)
-      }
-      if (stack === length) stack = 0;
-    }
+    else SlideTo(current, prev)
   }
 });
 
@@ -101,7 +60,8 @@ function changeIndicatorActive(curActive, nexActive) {
 }
 
 function SlideLeft(curActive, nexActive) {
-  $('.slide-item:first').before($('.slide-item:last'));
+  let clone = $('.slide-item:last').clone()
+  $('.slide-item:first').before(clone);
   carousel.css('transform', 'translateX(' + (-slideWidth) + 'px)');
   setTimeout(function () {
     carousel.addClass('transition');
@@ -109,19 +69,23 @@ function SlideLeft(curActive, nexActive) {
   })
   setTimeout(function () {
     carousel.removeClass('transition')
+    $('.slide-item:first').remove();
+    carousel.css('transform', 'translateX('+ -slideWidth*nexActive +'px)');
   }, 400)
   changeIndicatorActive(curActive, nexActive)
 }
 
 function SlideRight(curActive, nexActive) {
-  $('.slide-item:last').after($('.slide-item:first'));
-  carousel.css('transform', 'translateX(' + (-(length - 1) * slideWidth + slideWidth) + 'px)');
+  let clone = $('.slide-item:first').clone();
+  $('.slide-item:last').after(clone);
   setTimeout(function () {
     carousel.addClass('transition');
-    carousel.css('transform', 'translateX(' + (-(length - 1) * slideWidth) + 'px)');
+    carousel.css('transform', 'translateX('+ -(slideWidth*length) +'px)');
   })
   setTimeout(function () {
     carousel.removeClass('transition')
+    $('.slide-item:last').remove();
+    carousel.css('transform', 'translateX('+ -slideWidth*nexActive +'px)');
   }, 400)
   changeIndicatorActive(curActive, nexActive)
 }
